4b5287d86368eb9aad7ae9a425f9f5cb0b2ee878
Tom Bulsink <nospam@sorrowed.nl>
2018-12-17 21:45:47 +0100

    FEAT: Day08 part 2

--- src/day08/mod.py
+++ src/day08/mod.py
@@ -1,4 +1,6 @@
 def get_input():
+    # return [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
+
     with open("input.txt", "r") as f:
         return f.read().split()
 
@@ -9,12 +11,26 @@ class Node:
         self.cc = cc
         self.mc = mc
         self.meta = list()
+        self.children = list()
+
+    def get_value_pt1(self):
+        return sum(self.meta)
+
+    def get_value_pt2(self):
+        """
+        Return part 1 value if no children, else use meta content as child indices ( filter values to range
+        [1..len(self.children)] ) and sum their pt2 values. Return that
+        :return: Yes, when done.
+        """
+        return self.get_value_pt1() if len(self.children) == 0 else \
+            sum(child.get_value_pt2() for child in
+                [self.children[m - 1] for m in self.meta if 0 < m <= len(self.children)])
 
     def __str__(self):
         return "{3:03d}: {0:03d} {1:03d}->{2}".format(self.cc, self.mc, self.meta, self.nr)
 
 
-def parse_nodes(dst, lvl, inp):
+def parse_nodes(parent, dst, lvl, inp):
     """
     Recursively traverse the inp parsing nodes and putting them in dst
     :return: dst with added nodes and unparsed input
@@ -27,14 +43,17 @@ def parse_nodes(dst, lvl, inp):
     mc = int(inp[1])
 
     node = Node(lvl, cc, mc)
+    if parent is not None:
+        parent.children.append(node)
+
     dst.append(node)
 
     # Remove fixed piece and parse children if any
     inp = inp[2:]
     for c in range(0, cc):
-        dst, inp = parse_nodes(dst, lvl + 1, inp)
+        dst, inp = parse_nodes(node, dst, lvl + 1, inp)
 
-    # inp is unparsed input, so this contains the meta part we aer still missing
+    # inp is unparsed input, so this starts with the meta part we are still missing
     node.meta = [int(c) for c in inp[:mc]]
 
     # .. and remove the meta part
@@ -44,15 +63,17 @@ def parse_nodes(dst, lvl, inp):
 
 
 def first():
-    nodes, _ = parse_nodes(list(), 0, get_input())
+    nodes, _ = parse_nodes(None, list(), 0, get_input())
 
-    return sum(sum(node.meta) for node in nodes)
+    return sum(node.get_value_pt1() for node in nodes)
 
 
 def second():
-    return 0
+    nodes, _ = parse_nodes(None, list(), 0, get_input())
+
+    return nodes[0].get_value_pt2()
 
 
 if __name__ == "__main__":
     print("Meta sum : {0}".format(first()))
-    print("Correct order : {0}".format(second()))
+    print("Meta sum : {0}".format(second()))

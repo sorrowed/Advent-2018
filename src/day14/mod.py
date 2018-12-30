dd3c5fdfbedb6efe8608d2acf90b9c46f14bd472
Unknown <nospam@sorrowed.nl>
2018-12-30 15:14:45 +0100

    Day14 pt 2
    Using bytes/bytearray now because i thought i had a memory shortage. Turns out i had a PEBKAC because of not looking at the end of the recipes when adding 2 in one iteration.

--- src/day14/mod.py
+++ src/day14/mod.py
@@ -1,61 +1,72 @@
-def cooking(recipes, one, two):
-	sum = recipes[one] + recipes[two]
-	tens = int(sum / 10)
-	ones = int(sum % 10)
+def cooking(recipes, one, two, pattern=None):
+    sum = recipes[one] + recipes[two]
 
-	if tens > 0:
-		recipes.append(tens)
+    if sum >= 10:
+        recipes.append(1)
 
-	recipes.append(ones)
+    if pattern is not None and recipes.endswith(pattern):
+        return recipes, one, two
 
-	one = (one + 1 + recipes[one]) % len(recipes)
-	two = (two + 1 + recipes[two]) % len(recipes)
+    recipes.append(int(sum % 10))
+    if pattern is not None and recipes.endswith(pattern):
+        return recipes, one, two
 
-	return recipes, one, two
+    one = (one + 1 + recipes[one]) % len(recipes)
+    two = (two + 1 + recipes[two]) % len(recipes)
+
+    return recipes, one, two
 
 
 def cooking_count(count):
-	recipes = [3, 7]
+    recipes = bytearray([3, 7])
 
-	one = 0
-	two = 1
+    one = 0
+    two = 1
 
-	while len(recipes) < count:
-		recipes, one, two = cooking(recipes, one, two)
+    while len(recipes) < count:
+        recipes, one, two = cooking(recipes, one, two)
 
-	return recipes
+    return recipes
 
 
 def cooking_pattern(pattern):
-	recipes = [3, 7]
+    recipes = bytearray([3, 7])
 
-	one = 0
-	two = 1
+    one = 0
+    two = 1
 
-	while len(recipes) < 20:
-		recipes, one, two = cooking(recipes, one, two)
+    while not recipes.endswith(pattern):
+        recipes, one, two = cooking(recipes, one, two, pattern)
 
-	while len(recipes) < len(pattern) or pattern != "".join(str(n) for n in recipes[-len(pattern):]):
-		recipes, one, two = cooking(recipes, one, two)
-
-	return recipes
+    return recipes
 
 
 def test():
-	print(cooking_count(2018 + 10)[-10:])
+    assert cooking_count(2018 + 10).endswith(bytes([5, 9, 4, 1, 4, 2, 9, 8, 8, 2]))
+
+    p = bytes([5, 1, 5, 8, 9])
+    assert len(cooking_pattern(p)) - len(p) == 9
+
+    p = bytes([0, 1, 2, 4, 5])
+    assert len(cooking_pattern(p)) - len(p) == 5
+
+    p = bytes([9, 2, 5, 1, 0])
+    assert len(cooking_pattern(p)) - len(p) == 18
+
+    p = bytes([5, 9, 4, 1, 4])
+    assert len(cooking_pattern(p)) - len(p) == 2018
 
 
 def first():
-	return cooking_count(637061 + 10)[-10:]
+    return "".join(str(n) for n in cooking_count(637061 + 10)[-10:])
 
 
 def second():
-	pattern = "637061"
-
-	return len(cooking_pattern(pattern)) - len(pattern)
+    p = bytes([6, 3, 7, 0, 6, 1])
+    return len(cooking_pattern(p)) - len(p)
 
 
 if __name__ == "__main__":
-	test()
-	print("Recipes after input: {0}".format(first()))
-	print("Recipes before patter: {0}".format(second()))
+    test()
+    print("Recipes after input: {0}".format(first()))
+    print("Recipes before pattern: {0}".format(second()))

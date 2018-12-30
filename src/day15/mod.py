019adcf8c5b39f5576292515b4e1961a4ab121df
Tom Bulsink <nospam@sorrowed.nl>
2018-12-31 00:03:21 +0100

    WIP: Map/BF/Unit classes

--- src/day15/mod.py
+++ src/day15/mod.py
@@ -34,6 +34,43 @@ INPUT = ["################################",
          "################################"]
 
 
+class Map:
+	def __init__(self, map):
+		self.map, self.units = self.process(map)
+
+	def __getitem__(self, location):
+		return self.map[location.y][location.x]
+
+	def process(self, map):
+		m, units = list(), list()
+
+		for y in range(len(map)):
+			line = list()
+			for x in range(len(map[y])):
+				if map[y][x] == "#" or map[y][x] == ".":
+					line.append(map[y][x])
+				elif map[y][x] == "G" or map[y][x] == "E":
+					units.append(Unit(map[y][x], x, y))
+					line.append(".")
+
+			m.append(line)
+
+		return m, units
+
+	def neighbors(self, location):
+		n = list()
+		if self[location.above()] != "#":
+			n.append(location.above())
+		if self[location.right_of()] != "#":
+			n.append(location.right_of())
+		if self[location.below()] != "#":
+			n.append(location.below())
+		if self[location.left_of()] != "#":
+			n.append(location.left_of())
+
+		return n
+
+
 class Location:
 	def __init__(self, x, y):
 		self.x = x
@@ -59,31 +96,17 @@ class Location:
 
 
 class BreadthFirst:
-	def __init__(self, map):
-		self.map = map
+	def __init__(self, map, start):
+		self.start = start
+		self.search_map = self.calculate_search_map(map, start)
 
-	def __getitem__(self, location):
-		return self.map[location.y][location.x]
-
-	def neighbors(self, location):
-		n = list()
-		if self[location.above()] != "#":
-			n.append(location.above())
-		if self[location.right_of()] != "#":
-			n.append(location.right_of())
-		if self[location.below()] != "#":
-			n.append(location.below())
-		if self[location.left_of()] != "#":
-			n.append(location.left_of())
-
-		return n
-
-	def calculate_map(self, start, target=None):
+	@staticmethod
+	def calculate_search_map(map, start, target=None):
 		frontier = queue.Queue()
 		frontier.put(start)
 
-		map = dict()
-		map[start] = None
+		path_map = dict()
+		path_map[start] = None
 
 		while not frontier.empty():
 			current = frontier.get()
@@ -92,34 +115,31 @@ class BreadthFirst:
 			if target is not None and current == target:
 				break
 
-			for n in self.neighbors(current):
-				if n not in map:
+			for n in map.neighbors(current):
+				if n not in path_map:
 					frontier.put(n)
-					map[n] = current
+					path_map[n] = current
 
-		return map
+		return path_map
 
-	def get_path(self, map, start, target):
+	def get_path(self, target):
 		current = target
 
 		path = []
-		while current != start:
+		while current != self.start:
 			path.append(current)
-			current = map[current]
-		path.append(start)  # optional
+			current = self.search_map[current]
+		path.append(self.start)  # optional
 		path.reverse()  # optional
 
 		return path
 
 
 class Unit:
-	def __init__(self, x, y):
+	def __init__(self, type, x, y):
 		self.hp = 200
+		self.type = type
 		self.location = Location(x, y)
-		self.targets = list()
-
-	def targets(self, map):
-		pass
 
 
 def test():

80f8ac49d4cca5a985f3d1bf39e6cb809955c3cd
Unknown <nospam@sorrowed.nl>
2019-01-03 00:20:01 +0100

    Day15 pt 1
    Some details i got wrong:
    - BF map should prioritize neighbors in reading order (if more paths have the same length)
    - When multiple targets are available with the same path length, take the *target* first in reading order, not the first move
    - Units can move *and* attack in the same round

--- src/day15/mod.py
+++ src/day15/mod.py
@@ -71,24 +71,31 @@ class Map:
 
         return m, units
 
+    @staticmethod
+    def is_passable(c):
+        return c in [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
     def neighbors(self, location, target):
+
         # Can't move through units. Can move to target
         ul = [unit.location for unit in self.units if unit.location != target]
 
         n = list()
-        if location.above() not in ul and self[location.above()] == ".":
+        # Neighbors are added in reading order to prioritize those paths that have their first position
+        # first in reading order *if* there is more than one path to the target
+        if location.above() not in ul and Map.is_passable(self[location.above()]):
             n.append(location.above())
-        if location.right_of() not in ul and self[location.right_of()] == ".":
-            n.append(location.right_of())
-        if location.below() not in ul and self[location.below()] == ".":
-            n.append(location.below())
-        if location.left_of() not in ul and self[location.left_of()] == ".":
+        if location.left_of() not in ul and Map.is_passable(self[location.left_of()]):
             n.append(location.left_of())
+        if location.right_of() not in ul and Map.is_passable(self[location.right_of()]):
+            n.append(location.right_of())
+        if location.below() not in ul and Map.is_passable(self[location.below()]):
+            n.append(location.below())
 
         return n
 
     def targets(self, unit):
-        return [u for u in self.units if u.type != unit.type and not u.is_dead()]
+        return [u for u in self.units if u.unit_type != unit.unit_type and not u.is_dead()]
 
     def __str__(self):
         locations = {unit.location: unit for unit in self.units if not unit.is_dead()}
@@ -99,15 +106,14 @@ class Map:
             for x in range(len(line)):
                 location = Location(x, y)
                 if location in locations:
-                    s += locations[location].type
+                    s += locations[location].unit_type
                 else:
                     s += line[x]
 
             s += "\t"
 
-            for unit in self.units:
-                if unit.location.y == y:
-                    s += str(unit)
+            for unit in sorted([unit for unit in self.units if unit.location.y == y], key=lambda unit: unit.location):
+                s += str(unit)
 
             s += "\n"
 
@@ -160,6 +166,9 @@ class BreadthFirst:
         while not frontier.empty():
             current = frontier.get()
 
+            if current == self.target:
+                break
+
             for n in map.neighbors(current, self.target):
                 if n not in path_map:
                     frontier.put(n)
@@ -176,14 +185,12 @@ class BreadthFirst:
         path = list()
         try:
             while True:
-                # Not storing first current(==target) because we need to move beside the target, not on it
-                current = self.search_map[current]
-
-                # Not storing starting position either
+                # Not storing starting position, but we do store end/target position
                 if current != self.start:
                     path.append(current)
                 else:
                     break
+                current = self.search_map[current]
 
             path.reverse()
         except KeyError:  # No path exists
@@ -193,10 +200,10 @@ class BreadthFirst:
 
 
 class Unit:
-    def __init__(self, type, x, y):
+    def __init__(self, unit_type, x, y):
         self.hp = 200
         self.power = 3
-        self.type = type
+        self.unit_type = unit_type
         self.location = Location(x, y)
 
     def distance(self, other):
@@ -213,7 +220,7 @@ class Unit:
         return self.hp <= 0
 
     def __str__(self):
-        return "({0}:{1})".format(self.type, self.hp)
+        return "({0}:{1})".format(self.unit_type, self.hp)
 
 
 def find_victim(src, targets):
@@ -233,38 +240,63 @@ def process_round(map):
     map.units = [unit for unit in map.units if not unit.is_dead()]
 
     # Sort units in reading order based on location
-    map.units.sort(key=lambda u: u.location)
+    map.units.sort(key=lambda unit: unit.location)
 
-    for u in map.units:
-        print(u)
-        
     for unit in map.units:
 
+        # Skip if unit died this round before it had a chance to do something
         if unit.is_dead():
             continue
 
         targets = map.targets(unit)
 
+        # No targets found. Battle ends
         if len(targets) == 0:
             r = False
             break
 
+        # Find any victims to be attacked. If none found find path to nearest target and move
+        victim = find_victim(unit, targets)
+        if victim is None:
+            paths = [path for path in [BreadthFirst(map, unit.location, target.location).path() for target in targets]
+                     if len(path) > 0]
+
+            if len(paths) > 0:
+                # Shortest path is nearest target. If more of them exist, take the target first in reading order
+                # That target is at location path[-1]
+                path = sorted(paths, key=lambda path: (len(path), path[-1]))[0]
+
+                # And move....
+                unit.move(path[0])
+
+        # Find any victims to be attacked (again, maybe the unit moved into range of a target)
         victim = find_victim(unit, targets)
         if victim is not None:
             unit.attack(victim)
-        else:
-            paths = [BreadthFirst(map, unit.location, target.location).path() for target in
-                     targets]
-
-            # Filter empty paths.
-            nearest_path = [path for path in paths if len(path) > 0]
-            # Shortest path is nearest target. If more of them exist, take the target first in reading order
-            nearest_path.sort(key=lambda p: (len(p), p[0]))
-
-            if len(nearest_path) > 0:
-                unit.move(nearest_path[0][0])
 
     return r
+
+
+def battle(map_input, print_steps=False):
+    map = Map(map_input)
+    print("Start")
+    print(map)
+
+    rounds = 0
+
+    while True:
+        if not process_round(map):
+            print(map)
+            print("End")
+            break
+        else:
+            rounds += 1
+            if print_steps:
+                print(rounds)
+                print(map)
+            map.reset()
+
+    return rounds, sum([unit.hp for unit in map.units if not unit.is_dead()])
 
 
 def test():
@@ -275,28 +307,64 @@ def test():
                  "#..G#E#",
                  "#.....#",
                  "#######"]
+    rounds, hp = battle(map_input)
+    assert (rounds * hp == 27730)
 
-    map = Map(map_input)
-    print(map)
+    map_input = ["#######",
+                 "#G..#E#",
+                 "#E#E.E#",
+                 "#G.##.#",
+                 "#...#E#",
+                 "#...E.#",
+                 "#######"]
+    rounds, hp = battle(map_input)
+    assert (rounds * hp == 36334)
 
-    battle = True
-    rounds = 0
+    map_input = ["#######",
+                 "#E..EG#",
+                 "#.#G.E#",
+                 "#E.##E#",
+                 "#G..#.#",
+                 "#..E#.#",
+                 "#######"]
+    rounds, hp = battle(map_input)
+    assert (rounds * hp == 39514)
 
-    while battle:
-        if not process_round(map):
-            rounds -= 1  # Last target died in previous round
-            print(map)
-            break
-        else:
-            rounds += 1
-            print(rounds)
-            print(map)
+    map_input = ["#######",
+                 "#E.G#.#",
+                 "#.#G..#",
+                 "#G.#.G#",
+                 "#G..#.#",
+                 "#...E.#",
+                 "#######"]
+    rounds, hp = battle(map_input)
+    assert (rounds * hp == 27755)
 
-    return rounds, sum([unit.hp for unit in map.units if not unit.is_dead()])
+    map_input = ["#######",
+                 "#.E...#",
+                 "#.#..G#",
+                 "#.###.#",
+                 "#E#G#G#",
+                 "#...#G#",
+                 "#######"]
+    rounds, hp = battle(map_input)
+    assert (rounds * hp == 28944)
+
+    map_input = ["#########",
+                 "#G......#",
+                 "#.E.#...#",
+                 "#..##..G#",
+                 "#...##..#",
+                 "#...#...#",
+                 "#.G...G.#",
+                 "#.....G.#",
+                 "#########"]
+    rounds, hp = battle(map_input)
+    assert (rounds * hp == 18740)
 
 
 def first():
-    return 0
+    return battle(INPUT, True)
 
 
 def second():
@@ -304,8 +372,9 @@ def second():
 
 
 if __name__ == "__main__":
-    t = test()
-    print(t[0], t[1])
+    test()
 
-    print("Blaat: {0}".format(first()))
-    print("Blaat: {0}".format(second()))
+    rounds, hp = first()
+    print("Battled for {0} rounds and left with {1} HP for a total of {2}".format(rounds, hp, rounds * hp))
+    #
+    # print("Blaat: {0}".format(second()))

41ecbfbf8c112b8157c48792baab599b6dc3c6e4
Unknown <nospam@sorrowed.nl>
2019-02-10 13:58:54 +0100

    FEAT: Day20 part 1
    Use modified day 15 path finding to find paths between start and dead ends in maze.

--- src/day20/mod.py
+++ src/day20/mod.py
@@ -1,54 +1,199 @@
+import queue
+
+
 def get_input():
     with open("input.txt", "r") as f:
         return f.read()
 
 
-class Location():
+class BreadthFirst:
+    def __init__(self, map, start, target):
+        self.start = start
+        self.target = target
+        self.path_map = self.calculate_path_map(map)
+
+    def calculate_path_map(self, map):
+        frontier = queue.Queue()
+        frontier.put(self.start)
+
+        path_map = dict()
+        path_map[self.start] = None
+
+        while not frontier.empty():
+            current = frontier.get()
+
+            if current == self.target:
+                break
+
+            for n in map.neighbors(current):
+                if n not in path_map:
+                    frontier.put(n)
+                    path_map[n] = current
+
+        return path_map
+
+    def path(self):
+        """
+        Find path to target
+        """
+        current = self.target
+
+        path = list()
+        try:
+            while True:
+                current = self.path_map[current]
+
+                # Not storing start and target position
+                if current != self.start:
+                    path.append(current)
+                else:
+                    break
+
+            path.reverse()
+        except KeyError:  # No path exists
+            path.clear()
+
+        return path
+
+
+class Location:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def copy(self):
-        return Location(self.x, self.y)
+    def above(self):
+        return Location(self.x, self.y - 1)
+
+    def left_of(self):
+        return Location(self.x - 1, self.y)
+
+    def right_of(self):
+        return Location(self.x + 1, self.y)
+
+    def below(self):
+        return Location(self.x, self.y + 1)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __lt__(self, other):
+        return hash(self) < hash(other)
+
+    def __hash__(self):
+        return hash((self.y << 16) | self.x)
 
     def __str__(self):
         return "({0},{1})".format(self.x, self.y)
+
+    @staticmethod
+    def copy(location):
+        return Location(location.x, location.y)
 
 
 class Map:
     def __init__(self):
         self.map = dict()
-        self.map[(0, 0)] = 'X'
+        self.markers = list()
 
-    def __getitem__(self, c):
-        return self.map[(c[0], c[1])]
+    def __getitem__(self, location):
+        try:
+            return self.map[(location.x, location.y)]
+        except KeyError:
+            return "#"
 
-    def __setitem__(self, c, v):
-        self.map[(c[0], c[1])] = v
+    def __setitem__(self, location, v):
+        self.map[(location.x, location.y)] = v
 
-    def __str__(self):
+    def limits(self):
         x_min = min(self.map.keys(), key=lambda k: k[0])[0]
-        x_max = max(self.map.keys(), key=lambda k: k[0])[0]
         y_min = min(self.map.keys(), key=lambda k: k[1])[1]
+        x_max = max(self.map.keys(), key=lambda k: k[0])[0]
         y_max = max(self.map.keys(), key=lambda k: k[1])[1]
 
+        return Location(x_min, y_min), Location(x_max, y_max)
+
+    def __str__(self):
+        tl, br = self.limits()
+        de = self.dead_ends()
+
         s = str()
-        for y in range(y_min - 1, y_max + 2):  # Add border
+        for y in range(tl.y - 1, br.y + 2):  # Add border
             line = ""
-            for x in range(x_min - 1, x_max + 2):
-                if (x, y) in self.map:
-                    line += self.map[(x, y)]
+            for x in range(tl.x - 1, br.x + 2):
+
+                location = Location(x, y)
+
+                if location in de:
+                    line += "@"
+                elif location in self.markers:
+                    line += "*"
                 else:
-                    line += "#"
+                    line += self[location]
 
             s += line + "\n"
 
         return s
 
+    def start(self, location):
+        self[location] = 'X'
+
+    def add_markers(self, l):
+        self.markers.extend(l)
+
+    @staticmethod
+    def is_start(c):
+        return c == "X"
+
+    @staticmethod
+    def is_room(c):
+        return c == "." or Map.is_start(c)
+
+    @staticmethod
+    def is_door(c):
+        return c == "|" or c == "-"
+
+    def dead_ends(self):
+        """
+        Find all dead ends: All rooms with only one door neighbour
+        """
+        n = list()
+
+        for (x, y) in self.map:
+
+            location = Location(x, y)
+            c = self[location]
+            if Map.is_start(c) or (not Map.is_room(c)):
+                continue
+
+            if len(self.neighbor_doors(location)) == 1:
+                n.append(location)
+
+        return n
+
+    def neighbor_doors(self, location):
+        """
+        Returns all neighbor doors for location
+        """
+        n = [location.above(), location.right_of(), location.below(), location.left_of()]
+
+        return [l for l in n if Map.is_door(self[l])]
+
+    def neighbor_rooms(self, location):
+        """
+        Returns all neighbor rooms for location
+        """
+        n = [location.above(), location.right_of(), location.below(), location.left_of()]
+
+        return [l for l in n if Map.is_room(self[l])]
+
+    def neighbors(self, location):
+        return self.neighbor_doors(location) if \
+            Map.is_room(self[location]) else self.neighbor_rooms(location)
+
 
 class Path:
-    def __init__(self, x, y):
-        self.current = Location(x, y)
+    def __init__(self, location):
+        self.current = location
 
     def step(self, c, m):
 
@@ -56,62 +201,87 @@ class Path:
 
         if c == "E":
             current.x += 1
-            m[(current.x, current.y)] = "|"
+            m[current] = "|"
             current.x += 1
         elif c == "N":
             current.y -= 1
-            m[(current.x, current.y)] = "-"
+            m[current] = "-"
             current.y -= 1
         elif c == "W":
             current.x -= 1
-            m[(current.x, current.y)] = "|"
+            m[current] = "|"
             current.x -= 1
         elif c == "S":
             current.y += 1
-            m[(current.x, current.y)] = "-"
+            m[current] = "-"
             current.y += 1
 
-        m[(current.x, current.y)] = "."
+        m[current] = "."
 
 
 def traverse(inp, m):
     stack = list()  # Path stack
 
-    parent = Path(0, 0)
+    p = Path(Location(0, 0))
 
     for c in inp:
         if c == "^":
             pass
         elif c == "(":  # Start of path group, push parent on stack
-            child = Path(parent.current.x, parent.current.y)
-            stack.append(parent)
-            parent = child
+            child = Path(Location.copy(p.current))
+            stack.append(p)
+            p = child
         elif c == "|":  # End of child path. Continue with another child path in group, use same parent
-            parent = stack.pop()
-            child = Path(parent.current.x, parent.current.y)
-            stack.append(parent)
-            parent = child
-        elif c == ")" or c == "$":  # End of path group.
+            p = stack.pop()
+            child = Path(Location.copy(p.current))
+            stack.append(p)
+            p = child
+        elif c == ")" or c == "$":  # End of path group. Pop parent (if any) from stack. (This might discard an empty child path)
             if len(stack) > 0:
-                parent = stack.pop()
+                p = stack.pop()
         else:
-            parent.step(c, m)  # Add step to current path
+            p.step(c, m)  # Add step to current path
 
 
 def test():
-    inp = r"^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$"
+    inp = [r"^WNE$", r"^ENWWW(NEEE|SSE(EE|N))$", r"^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$",
+           r"^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))",
+           r"^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$"]
 
-    m = Map()
-    traverse(iter(inp), m)
-    print(m)
+    for i in inp:
+        m = Map()
+        m.start(Location(0, 0))
+
+        traverse(iter(i), m)
+
+        # Find paths from start position to all dead ends
+        paths = [BreadthFirst(m, Location(0, 0), de).path() for de in m.dead_ends()]
+
+        # Find the longest path, and only remember the doors (starts at first position with one room in between)
+        doors = max(paths, key=lambda path: len(path))[0::2]
+
+        m.add_markers(doors)
+
+        print(m, len(doors))
 
 
 def first():
     m = Map()
+    m.start(Location(0, 0))
 
     traverse(get_input(), m)
-    print(m)
-    return 0
+
+    # Find paths from start position to all dead ends
+    paths = [BreadthFirst(m, Location(0, 0), de).path() for de in m.dead_ends()]
+
+    # Find the longest path, remember doors only (first door is at first position, one room in between doors)
+    doors = max(paths, key=lambda path: len(path))[0::2]
+
+    m.add_markers(doors)
+
+    print(m, len(doors))
+
+    return len(doors)
 
 
 def second():
@@ -120,6 +290,5 @@ def second():
 
 if __name__ == "__main__":
     test()
-    print("Number of doors to furthest room: {0}".format(first()))
-
+    # print("Number of doors to furthest room: {0}".format(first()))
     # print("Dunno: {0}".format(second()))

c3624ca9c62b4e800da71ad4591602bf077cb6f8
Unknown <nospam@sorrowed.nl>
2019-02-17 13:04:24 +0100

    FEAT: Day23 part 1

---
+++ src/day23/mod.py
@@ -0,0 +1,72 @@
+import re
+
+parse_nanobot = re.compile(r"pos=<(-?\d+),(-?\d+),(-?\d+)>, r=(\d+)")
+
+
+class Nanobot:
+    def __init__(self, x, y, z, r):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.r = r
+
+    def in_range(self, other):
+        return self.distance(other) <= self.r  # and other.distance(self) <= other.r
+
+    def distance(self, other):
+        return abs(other.x - self.x) + abs(other.y - self.y) + abs(other.z - self.z)
+
+    def __str__(self):
+        return "({0},{1},{2})->{3}".format(self.x, self.y, self.z, self.r)
+
+
+def get_input():
+    with open("input.txt", "r") as f:
+        return f.readlines()
+
+
+def parse_nanobot_data(s):
+    matches = parse_nanobot.match(s)
+    return Nanobot(int(matches.group(1)), int(matches.group(2)), int(matches.group(3)), int(matches.group(4)))
+
+
+def test():
+    inp = ["pos=<0,0,0>, r=4", "pos=<1,0,0>, r=1", "pos=<4,0,0>, r=3",
+           "pos=<0,2,0>, r=1", "pos=<0,5,0>, r=3", "pos=<0,0,3>, r=1",
+           "pos=<1,1,1>, r=1", "pos=<1,1,2>, r=1", "pos=<1,3,1>, r=1"]
+
+    nanobots = [parse_nanobot_data(s) for s in inp]
+
+    strongest = max(nanobots, key=lambda nb: nb.r)
+
+    in_range = [nb for nb in nanobots if strongest.in_range(nb)]
+
+    print("Strongest nanobot:", strongest, "and", len(in_range), "in range")
+
+
+def first():
+    inp = get_input()
+    nanobots = [parse_nanobot_data(s) for s in inp]
+
+    strongest = max(nanobots, key=lambda nb: nb.r)
+
+    print("Strongest nanobot: ", strongest)
+
+    in_range = [nb for nb in nanobots if strongest.in_range(nb)]
+
+    return len(in_range)
+
+
+def second():
+    """
+    - Determine bounding box of all nanobots
+    - For each coordinate inside that box determine how many bots are in range, order descending
+    - Keep the one closest to (0,0,0)
+    """
+    pass
+
+
+if __name__ == "__main__":
+    test()
+    print("Nanobots in range of strongest: {0}".format(first()))
+    print("Bieb".format(second()))
